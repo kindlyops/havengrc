b653ddf11463f863d1b59d7a4f2aad618547b051
Elliot Murphy <statik@users.noreply.github.com>
2016-10-28 13:34:29 -0400

    Added elm-mdl via getdrip tutorial.
    https://www.dailydrip.com/topics/elm/drips/elm-mdl-introduction
    
    Signed-off-by: Elliot Murphy <elliot@elliotmurphy.com>

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,180 @@
+{- This file re-implements the Elm Counter example (1 counter) with elm-mdl
+   buttons. Use this as a starting point for using elm-mdl components in your own
+   app.
+-}
+
+
 module Main exposing (..)
 
-import App exposing (..)
-import Html.App exposing (program)
+import Html.App as App
+import Html exposing (..)
+import Html.Attributes exposing (href, class, style)
+import Material
+import Material.Scheme
+import Material.Button as Button
+import Material.Color as Color
+import Material.Layout as Layout
+import Material.Options exposing (css)
+
+
+-- MODEL
+-- You have to add a field to your model where you track the `Material.Model`.
+-- This is referred to as the "model container"
+
+
+type alias Model =
+    { count : Int
+    , mdl :
+        Material.Model
+        -- Boilerplate: model store for any and all Mdl components you use.
+    , selectedTab : Int
+    }
+
+
+
+-- `Material.model` provides the initial model
+
+
+model : Model
+model =
+    { count = 0
+    , mdl =
+        Material.model
+        -- Boilerplate: Always use this initial Mdl model store.
+    , selectedTab = 0
+    }
+
+
+
+-- ACTION, UPDATE
+-- You need to tag `Msg` that are coming from `Mdl` so you can dispatch them
+-- appropriately.
+
+
+type Msg
+    = Increase
+    | Reset
+    | Mdl (Material.Msg Msg)
+    | SelectTab Int
+
+
+
+-- Boilerplate: Msg clause for internal Mdl messages.
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Increase ->
+            ( { model | count = model.count + 1 }
+            , Cmd.none
+            )
+
+        Reset ->
+            ( { model | count = 0 }
+            , Cmd.none
+            )
+
+        SelectTab num ->
+            let
+                _ =
+                    Debug.log "SelectTab: " num
+            in
+                { model | selectedTab = num } ! []
+
+        -- When the `Mdl` messages come through, update appropriately.
+        Mdl msg' ->
+            Material.update msg' model
+
+
+
+-- VIEW
+
+
+type alias Mdl =
+    Material.Model
+
+
+view : Model -> Html Msg
+view model =
+    Material.Scheme.topWithScheme Color.Teal Color.LightGreen <|
+        Layout.render Mdl
+            model.mdl
+            [ Layout.fixedHeader
+            , Layout.selectedTab model.selectedTab
+            , Layout.onSelectTab SelectTab
+            ]
+            { header = [ h1 [ style [ ( "padding", "2rem" ) ] ] [ text "Counter" ] ]
+            , drawer = []
+            , tabs = ( [ text "Controls", text "Activities" ], [ Color.background (Color.color Color.Teal Color.S400) ] )
+            , main = [ viewBody model ]
+            }
+
+
+viewBody : Model -> Html Msg
+viewBody model =
+    case model.selectedTab of
+        0 ->
+            viewCounter model
+
+        1 ->
+            text "something else"
+
+        _ ->
+            text "not found fake 404"
+
+
+viewCounter : Model -> Html Msg
+viewCounter model =
+    div
+        [ style [ ( "padding", "2rem" ) ] ]
+        [ text ("Current count: " ++ toString model.count)
+          {- We construct the instances of the Button component that we need, one
+             for the increase button, one for the reset button. First, the increase
+             button. The first three arguments are:
+
+               - A Msg constructor (`Mdl`), lifting Mdl messages to the Msg type.
+               - An instance id (the `[0]`). Every component that uses the same model
+                 collection (model.mdl in this file) must have a distinct instance id.
+               - A reference to the elm-mdl model collection (`model.mdl`).
+
+             Notice that we do not have to add fields for the increase and reset buttons
+             separately to our model; and we did not have to add to our update messages
+             to handle their internal events.
+
+             Mdl components are configured with `Options`, similar to `Html.Attributes`.
+             The `Button.onClick Increase` option instructs the button to send the `Increase`
+             message when clicked. The `css ...` option adds CSS styling to the button.
+             See `Material.Options` for details on options.
+          -}
+        , Button.render Mdl
+            [ 0 ]
+            model.mdl
+            [ Button.onClick Increase
+            , css "margin" "0 24px"
+            ]
+            [ text "Increase" ]
+        , Button.render Mdl
+            [ 1 ]
+            model.mdl
+            [ Button.onClick Reset ]
+            [ text "Reset" ]
+        ]
+
+
+
+-- Load Google Mdl CSS. You'll likely want to do that not in code as we
+-- do here, but rather in your master .html file. See the documentation
+-- for the `Material` module for details.
 
 
 main : Program Never
 main =
-    program { view = view, init = init, update = update, subscriptions = subscriptions }
+    App.program
+        { init = ( model, Cmd.none )
+        , view =
+            view
+            -- Here we've added no subscriptions, but we'll need to use the `Mdl` subscriptions for some components later.
+        , subscriptions = always Sub.none
+        , update = update
+        }
