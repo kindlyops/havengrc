ff2e5e23e2d8435bb40a9b750242da11a20c23e4
Elliot Murphy <statik@users.noreply.github.com>
2017-05-27 23:53:55 +0100

    Fix flicker during load on Safari.
    Signed-off-by: Elliot Murphy <elliot@elliotmurphy.com>

--- web/src/index.js
+++ web/src/index.js
@@ -19,48 +19,49 @@ var authData = storedProfile && storedToken ? { profile: JSON.parse(storedProfil
 window.addEventListener('WebComponentsReady', function() {
   // At this point we are guaranteed that all required polyfills have loaded,
   // all HTML imports have loaded, and all defined custom elements have upgraded
-  // TODO We have a problem with flickering caused by elm
+
+  // We delay Elm.Main.embed because of a problem with flickering caused by elm
   // emitting webcomponent tags before the webcomponent system is ready.
   // unrecognized tags are simply rendered as an empty div by browers.
-});
+  var elmApp = Elm.Main.embed(document.getElementById('root'), authData);
 
-var elmApp = Elm.Main.embed(document.getElementById('root'), authData);
+  options = { redirect_uri: 'http://localhost:2015/' };
+  keycloak.init(options);
 
-options = { redirect_uri: 'http://localhost:2015/' };
-keycloak.init(options);
+  keycloak.onAuthSuccess = function() {
+    console.log("success from keycloak login");
+    var result = { err: null, ok: null };
+    keycloak.loadUserProfile().success(function() {
+          console.log("success from keycloak.loadUserProfile");
+          console.log(keycloak.profile);
+          localStorage.setItem('profile', JSON.stringify(keycloak.profile));
+          localStorage.setItem('token', keycloak.token);
+          result.ok = { profile: keycloak.profile, token: keycloak.token };
+          console.log("sending result ");
+          console.log(result);
+          elmApp.ports.keycloakAuthResult.send(result);
+      }).error(function(errorData) {
+          result.err = { name: "unknown error" };
+          // check for error, error_description
+          // https://github.com/keycloak/keycloak-js-bower/blob/master/dist/keycloak.js#L506
+          elmApp.ports.keycloakAuthResult.send(result);
+    });
+  };
 
-keycloak.onAuthSuccess = function() {
-  console.log("success from keycloak login");
-  var result = { err: null, ok: null };
-  keycloak.loadUserProfile().success(function() {
-        console.log("success from keycloak.loadUserProfile");
-        console.log(keycloak.profile);
-        localStorage.setItem('profile', JSON.stringify(keycloak.profile));
-        localStorage.setItem('token', keycloak.token);
-        result.ok = { profile: keycloak.profile, token: keycloak.token };
-        console.log("sending result ");
-        console.log(result);
-        elmApp.ports.keycloakAuthResult.send(result);
-    }).error(function(errorData) {
-        result.err = { name: "unknown error" };
-        // check for error, error_description
-        // https://github.com/keycloak/keycloak-js-bower/blob/master/dist/keycloak.js#L506
-        elmApp.ports.keycloakAuthResult.send(result);
+  elmApp.ports.keycloakShowLock.subscribe(function(opts) {
+    console.log("calling login");
+    keycloak.login().error(function(errorData) {
+      alert('failed to initialize'); // TODO polish this error case
+      // check for error, error_description
+      // https://github.com/keycloak/keycloak-js-bower/blob/master/dist/keycloak.js#L506
+    });
   });
-};
 
-elmApp.ports.keycloakShowLock.subscribe(function(opts) {
-  console.log("calling login");
-  keycloak.login().error(function(errorData) {
-    alert('failed to initialize'); // TODO polish this error case
-    // check for error, error_description
-    // https://github.com/keycloak/keycloak-js-bower/blob/master/dist/keycloak.js#L506
+  // Log out of keycloak
+  elmApp.ports.keycloakLogout.subscribe(function(opts) {
+    localStorage.removeItem('profile');
+    localStorage.removeItem('token');
+    keycloak.logout();
   });
-});
 
-// Log out of keycloak
-elmApp.ports.keycloakLogout.subscribe(function(opts) {
-  localStorage.removeItem('profile');
-  localStorage.removeItem('token');
-  keycloak.logout();
 });
