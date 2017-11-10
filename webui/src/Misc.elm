7c76e7d431e0e4276a4fb9a7678abb2f08c91199
Elliot Murphy <statik@users.noreply.github.com>
2017-11-10 13:23:26 -0500

    WIP
    Signed-off-by: Elliot Murphy <statik@users.noreply.github.com>

--- webui/src/Misc.elm
+++ webui/src/Misc.elm
@@ -10,6 +10,12 @@ import Types exposing (Model, Msg)
     (,)
 
 
+{-| infixl 0 means the (=>) operator has the same precedence as (<|) and (|>),
+meaning you can use it at the end of a pipeline and have the precedence work out.
+-}
+infixl 0 =>
+
+
 showDebugData : record -> Html Msg
 showDebugData record =
     div [ class "debug" ] [ text ("DEBUG: " ++ toString record) ]
