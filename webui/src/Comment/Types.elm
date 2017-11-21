2fd036c44fed881bdd3e07556ae6c377b86dd3f6
Elliot Murphy <statik@users.noreply.github.com>
2017-11-20 21:28:30 -0500

    Record stable keycloak user ID in addition to email.
    Signed-off-by: Elliot Murphy <statik@users.noreply.github.com>

--- webui/src/Comment/Types.elm
+++ webui/src/Comment/Types.elm
@@ -5,10 +5,11 @@ type alias Comment =
     { uuid : String
     , created_at : String
     , user_email : String
+    , user_id : String
     , message : String
     }
 
 
 emptyNewComment : Comment
 emptyNewComment =
-    Comment "" "" "" ""
+    Comment "" "" "" "" ""
