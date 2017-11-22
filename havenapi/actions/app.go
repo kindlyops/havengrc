c8badd32687e6db91769bed09d981bf5a823a5f3
Elliot Murphy <statik@users.noreply.github.com>
2017-11-21 22:21:26 -0500

    Initial skeleton of  gobuffalo based API server.
    Builds with docker build -t havenapi havenapi/
    
    Still needs wiring to docker-compose and a hot reload dockerfile.
    
    Signed-off-by: Elliot Murphy <statik@users.noreply.github.com>

---
+++ havenapi/actions/app.go
@@ -0,0 +1,47 @@
+package actions
+
+import (
+	"github.com/gobuffalo/buffalo"
+	"github.com/gobuffalo/buffalo/middleware"
+	"github.com/gobuffalo/buffalo/middleware/ssl"
+	"github.com/gobuffalo/envy"
+	"github.com/unrolled/secure"
+
+	"github.com/gobuffalo/x/sessions"
+)
+
+// ENV is used to help switch settings based on where the
+// application is being run. Default is "development".
+var ENV = envy.Get("GO_ENV", "development")
+var app *buffalo.App
+
+// App is where all routes and middleware for buffalo
+// should be defined. This is the nerve center of your
+// application.
+func App() *buffalo.App {
+	if app == nil {
+		app = buffalo.New(buffalo.Options{
+			Env:          ENV,
+			SessionStore: sessions.Null{},
+			SessionName:  "_havenapi_session",
+		})
+		// Automatically redirect to SSL
+		app.Use(ssl.ForceSSL(secure.Options{
+			SSLRedirect:     ENV == "production",
+			SSLProxyHeaders: map[string]string{"X-Forwarded-Proto": "https"},
+		}))
+
+		// Set the request content type to JSON
+		app.Use(middleware.SetContentType("application/json"))
+
+		if ENV == "development" {
+			app.Use(middleware.ParameterLogger)
+		}
+
+		app.GET("/", HomeHandler)
+		app.GET("/foo", HomeHandler)
+
+	}
+
+	return app
+}
