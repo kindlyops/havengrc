afb8cd5a64b7d1631bdb96e968280a602bc8a970
Elliot Murphy <statik@users.noreply.github.com>
2017-11-22 23:13:01 -0500

    Hook up buffalo tests in circleCI
    Signed-off-by: Elliot Murphy <statik@users.noreply.github.com>

--- havenapi/actions/home_test.go
+++ havenapi/actions/home_test.go
@@ -3,5 +3,5 @@ package actions
 func (as *ActionSuite) Test_HomeHandler() {
 	res := as.JSON("/").Get()
 	as.Equal(200, res.Code)
-	as.Contains(res.Body.String(), "Welcome to Buffalo")
+	as.Contains(res.Body.String(), "Welcome to HavenGRC")
 }
