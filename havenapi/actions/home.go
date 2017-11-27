909f1d83f1e4eb5119b25b097b3664b0394bce93
Elliot Murphy <statik@users.noreply.github.com>
2017-11-26 21:54:42 -0500

    initial DB access working on new go api server Signed-off-by: Elliot Murphy <statik@users.noreply.github.com>

--- havenapi/actions/home.go
+++ havenapi/actions/home.go
@@ -1,9 +1,24 @@
 package actions
 
-import "github.com/gobuffalo/buffalo"
+import (
+	"fmt"
+
+	"github.com/gobuffalo/buffalo"
+	"github.com/kindlyops/mappamundi/havenapi/models"
+)
 
 // HomeHandler is a default handler to serve up
 // a home page.
 func HomeHandler(c buffalo.Context) error {
-	return c.Render(200, r.JSON(map[string]string{"message": "Welcome to HavenGRC!"}))
+	err := models.DB.RawQuery("set search_path to 'mappa'").Exec()
+	if err != nil {
+		return c.Error(500, fmt.Errorf("Database error: %s", err.Error()))
+	}
+	var count []int
+	err = models.DB.RawQuery("select count(*) from comment").All(&count)
+	if err != nil {
+		return c.Error(500, fmt.Errorf("Database error: %s", err.Error()))
+	}
+	message := fmt.Sprintf("Welcome to HavenGRC! There are %d comments", count[0])
+	return c.Render(200, r.JSON(map[string]string{"message": message}))
 }
---
+++ havenapi/models/models.go
@@ -0,0 +1,23 @@
+package models
+
+import (
+	"log"
+
+	"github.com/gobuffalo/envy"
+	"github.com/markbates/pop"
+)
+
+// DB is a connection to your database to be used
+// throughout your application.
+var DB *pop.Connection
+
+func init() {
+	var err error
+	env := envy.Get("GO_ENV", "development")
+	DB, err = pop.Connect(env)
+	log.Println("models.init() was called")
+	if err != nil {
+		log.Fatal(err)
+	}
+	pop.Debug = env == "development"
+}
