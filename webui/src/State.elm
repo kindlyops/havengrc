4a309a4e9dd94b37fed1e0bad7da1db0cca95b72
Elliot Murphy <statik@users.noreply.github.com>
2018-03-30 23:50:12 -0400

    more incremental fixes.
    Signed-off-by: Elliot Murphy <statik@users.noreply.github.com>

--- webui/src/State.elm
+++ webui/src/State.elm
@@ -6,10 +6,12 @@ import Keycloak
 import Navigation
 import Ports
 import Page.Comments
+import Page.Errored as Errored
 import Comment.Rest exposing (getComments, postComment)
 import Comment.Types exposing (Comment, emptyNewComment)
 import Route
 import Types exposing (..)
+import Misc exposing (..)
 
 
 init : Maybe Keycloak.LoggedInUser -> Navigation.Location -> ( Model, Cmd Msg )
@@ -55,56 +57,64 @@ getPage pageState =
             page
 
 
+pageErrored model activePage errorMessage =
+    let
+        error =
+            Errored.pageLoadError errorMessage
+    in
+        { model | pageState = Loaded (Errored error) } => Cmd.none
+
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     updatePage (getPage model.pageState) msg model
 
 
 updatePage : Page -> Msg -> Model -> ( Model, Cmd Msg )
-update page msg model =
+updatePage page msg model =
     let
         session =
             model.authModel
 
         toPage toModel toMsg subUpdate subMsg subModel =
             let
-                (newModel, newCmd ) = 
+                ( newModel, newCmd ) =
                     subUpdate subMsg subModel
             in
-            ( { model | pageState = Loaded(toModel newModel) }, Cmd.map toMsg newCmd )
-        
+                ( { model | pageState = Loaded (toModel newModel) }, Cmd.map toMsg newCmd )
+
         errored =
             pageErrored model
     in
-    case ( msg, page ) of
-        ( Types.AuthenticationMsg authMsg, _ ) ->
-            let
-                ( authModel, cmd ) =
-                    Authentication.update authMsg model.authModel
-            in
-                ( { model | authModel = authModel }, Cmd.map Types.AuthenticationMsg cmd )
+        case ( msg, page ) of
+            ( Types.AuthenticationMsg authMsg, _ ) ->
+                let
+                    ( authModel, cmd ) =
+                        Authentication.update authMsg model.authModel
+                in
+                    ( { model | authModel = authModel }, Cmd.map Types.AuthenticationMsg cmd )
 
-        ( NavigateTo maybeLocation, _ ) ->
-            (transitionTo maybeLocation model)
+            ( NavigateTo maybeLocation, _ ) ->
+                (transitionTo maybeLocation model)
 
-        ( UrlChange location, _ ) ->
-            let
-                _ =
-                    Debug.log "UrlChange: " location.hash
-            in
-                { model | route = Route.locFor (Just location) } ! []
+            ( UrlChange location, _ ) ->
+                let
+                    _ =
+                        Debug.log "UrlChange: " location.hash
+                in
+                    { model | route = Route.locFor (Just location) } ! []
 
-        ( CommentsMsg user commentMsg,  ->
-            case Authentication.tryGetUserProfile model.authModel of
-                Nothing ->
-                    -- this should never happen
-                    ( model, Cmd.none )
+            ( CommentsMsg user subMsg, subModel ) ->
+                case Authentication.tryGetUserProfile model.authModel of
+                    Nothing ->
+                        -- this should never happen
+                        ( model, Cmd.none )
 
-                Just user ->
-                    Page.Comments.update commentMsg user model
+                    Just user ->
+                        toPage Comment CommentsMsg (Page.Comments.update subMsg user subModel)
 
-        ShowError value ->
-            model ! [ Ports.showError value ]
+            ShowError value ->
+                model ! [ Ports.showError value ]
 
 
 subscriptions : a -> Sub Msg
