4e26a0b2e6d348d1b88f7c59bb914d4630b5966d
David Streeter <dstreet26@gmail.com>
2018-05-01 22:40:34 -0400

    standardformatjs run on index.js
    Signed-off-by: David Streeter <dstreet26@gmail.com>

--- webui/src/index.js
+++ webui/src/index.js
@@ -1,25 +1,25 @@
 // import "./main2.scss";
-import { Main } from "./Main.elm";
+import { Main } from './Main.elm'
 
-if (process.env.NODE_ENV === "development") {
-  var CLIENT_ID = process.env.ELM_APP_KEYCLOAK_CLIENT_ID;
+if (process.env.NODE_ENV === 'development') {
+  var CLIENT_ID = process.env.ELM_APP_KEYCLOAK_CLIENT_ID
 } else {
-  var CLIENT_ID = "{{.Env.ELM_APP_KEYCLOAK_CLIENT_ID}}";
+  var CLIENT_ID = '{{.Env.ELM_APP_KEYCLOAK_CLIENT_ID}}'
 }
-/*global Keycloak*/
+/* global Keycloak */
 var keycloak = Keycloak({
-  url: "/auth",
-  realm: "havendev",
-  clientId: "havendev"
-});
-var storedProfile = localStorage.getItem("profile");
-var storedToken = localStorage.getItem("token");
+  url: '/auth',
+  realm: 'havendev',
+  clientId: 'havendev'
+})
+var storedProfile = localStorage.getItem('profile')
+var storedToken = localStorage.getItem('token')
 var authData =
   storedProfile && storedToken
     ? { profile: JSON.parse(storedProfile), token: storedToken }
-    : null;
+    : null
 
-var elmApp = Main.embed(document.getElementById("root"), authData);
+var elmApp = Main.embed(document.getElementById('root'), authData)
 
 // document.arrive(".mdc-textfield", function() {
 //   window.mdc.autoInit(document, () => {});
@@ -81,67 +81,67 @@ var elmApp = Main.embed(document.getElementById("root"), authData);
 //   }
 // });
 
-function sendElmKeycloakToken() {
-  localStorage.setItem("profile", JSON.stringify(keycloak.profile));
-  localStorage.setItem("token", keycloak.token);
-  var result = { err: null, ok: null };
-  result.ok = { profile: keycloak.profile, token: keycloak.token };
-  elmApp.ports.keycloakAuthResult.send(result);
+function sendElmKeycloakToken () {
+  localStorage.setItem('profile', JSON.stringify(keycloak.profile))
+  localStorage.setItem('token', keycloak.token)
+  var result = { err: null, ok: null }
+  result.ok = { profile: keycloak.profile, token: keycloak.token }
+  elmApp.ports.keycloakAuthResult.send(result)
 }
 
-keycloak.onAuthSuccess = function() {
+keycloak.onAuthSuccess = function () {
   keycloak
     .loadUserProfile()
     .success(sendElmKeycloakToken)
-    .error(function(/*errorData*/) {
-      var result = { err: { name: "unknown error" }, ok: null };
-      elmApp.ports.keycloakAuthResult.send(result);
-    });
-};
-
-function tellElmLogout() {
-  ///alert("got keycloak logout callback");
-  // send a null result to trigger our existing AuthResult code and logout.
-  var result = { err: null, ok: null };
-  localStorage.removeItem("profile");
-  localStorage.removeItem("token");
-  elmApp.ports.keycloakAuthResult.send(result);
+    .error(function (/* errorData */) {
+      var result = { err: { name: 'unknown error' }, ok: null }
+      elmApp.ports.keycloakAuthResult.send(result)
+    })
 }
 
-keycloak.onAuthLogout = tellElmLogout;
+function tellElmLogout () {
+  /// alert("got keycloak logout callback");
+  // send a null result to trigger our existing AuthResult code and logout.
+  var result = { err: null, ok: null }
+  localStorage.removeItem('profile')
+  localStorage.removeItem('token')
+  elmApp.ports.keycloakAuthResult.send(result)
+}
 
-keycloak.onAuthRefreshSuccess = sendElmKeycloakToken;
+keycloak.onAuthLogout = tellElmLogout
 
-keycloak.onAuthRefreshError = tellElmLogout;
+keycloak.onAuthRefreshSuccess = sendElmKeycloakToken
 
-keycloak.onTokenExpired = function() {
-  console.log("got keycloak token expired");
-  keycloak.updateToken(300);
-};
+keycloak.onAuthRefreshError = tellElmLogout
 
-keycloak.init();
+keycloak.onTokenExpired = function () {
+  console.log('got keycloak token expired')
+  keycloak.updateToken(300)
+}
 
-elmApp.ports.keycloakLogin.subscribe(function() {
-  keycloak.login().error(function(/*errorData*/) {
-    alert("failed to initialize");
+keycloak.init()
+
+elmApp.ports.keycloakLogin.subscribe(function () {
+  keycloak.login().error(function (/* errorData */) {
+    alert('failed to initialize')
     // check for error, error_description
     // https://github.com/keycloak/keycloak-js-bower/blob/master/dist/keycloak.js#L506
-  });
-});
+  })
+})
 
 // Log out of keycloak
-elmApp.ports.keycloakLogout.subscribe(function() {
-  localStorage.removeItem("profile");
-  localStorage.removeItem("token");
-  keycloak.logout();
-});
+elmApp.ports.keycloakLogout.subscribe(function () {
+  localStorage.removeItem('profile')
+  localStorage.removeItem('token')
+  keycloak.logout()
+})
 
 // set the page title
-elmApp.ports.setTitle.subscribe(function(title) {
-  document.title = title;
-});
+elmApp.ports.setTitle.subscribe(function (title) {
+  document.title = title
+})
 
-elmApp.ports.showError.subscribe(function(messageString) {
+elmApp.ports.showError.subscribe(function (messageString) {
   console.info('TODO: add errors to snackbar')
   console.error(messageString)
   // let item = document.querySelector(".mdc-snackbar");
@@ -149,4 +149,4 @@ elmApp.ports.showError.subscribe(function(messageString) {
   //   let snack = mdc.snackbar.MDCSnackbar.attachTo(item);
   //   snack.show({ message: messageString });
   // }
-});
+})
