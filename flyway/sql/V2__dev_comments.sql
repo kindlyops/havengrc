9306e48e94ef7e11a2377105118f77222f9491b8
David Streeter <dstreet26@gmail.com>
2018-05-03 21:59:46 -0400

    moving the grant-view-usage-postgrest stuff away from the comments definition
    Signed-off-by: David Streeter <dstreet26@gmail.com>

--- flyway/sql/V2__dev_comments.sql
+++ flyway/sql/V2__dev_comments.sql
@@ -2,15 +2,7 @@
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
--- the schema "1" is our versioned public API
--- the schema mappa is private
--- grants are done to the private schema "mappa" because
--- the view needs to read from that schema with the role
--- of the authenticated caller
 
-
-GRANT usage ON schema mappa to member;
-GRANT usage ON schema "1" to member;
 
 -- for these three columns we never let the caller control the contents
 CREATE OR REPLACE FUNCTION mappa.override_server_columns()
