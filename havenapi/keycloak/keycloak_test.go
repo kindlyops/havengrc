3c74d8823e6f0b9f0f62d65203e44cd0e0f138eb
Christopher Mundus <chris@kindlyops.com>
2018-10-02 17:50:11 -0400

    Added test for GetUser func.
    Now tests GetUser function.
    
    Issue #HAV-137
    
    Signed-off-by: Christopher Mundus <chris@kindlyops.com>

--- havenapi/keycloak/keycloak_test.go
+++ havenapi/keycloak/keycloak_test.go
@@ -10,3 +10,14 @@ func TestGetToken(t *testing.T) {
 		t.Errorf("Failed test because: %s", err.Error())
 	}
 }
+
+func TestGetUser(t *testing.T) {
+	users, err := GetUser("user1@havengrc.com")
+	if err != nil {
+		t.Errorf("Failed test because: %s and %s", err.Error(), users)
+	}
+	if len(users) == 0 {
+		t.Errorf("Failed and found %s", users)
+	}
+
+}
