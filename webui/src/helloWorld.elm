0aa08b389fd1ff03821a9345283ad5d4cd0d06da
Christopher Mundus <chris@kindlyops.com>
2018-10-17 14:47:21 -0400

    Refactor to add actual names and getData func.
    Will need additional work on getData.
    
    Issue #HAV-113
    
    Signed-off-by: Christopher Mundus <chris@kindlyops.com>

--- webui/src/helloWorld.elm
+++ webui/src/helloWorld.elm
@@ -2,18 +2,48 @@ port module HelloWorld exposing (elmToJS)
 
 import Platform
 import VegaLite exposing (..)
+import Data.Survey
+
+getData : Data.Survey.Model -> List Data.Survey.IpsativeSingleResponse
+getData model =
+    let
+        allResponses =
+            case model.currentSurvey of
+                Data.Survey.Ipsative survey ->
+                    Data.Survey.getAllResponsesFromIpsativeSurvey survey
+                Data.Survey.Likert survey ->
+                    []
+        _=
+            Debug.log("Test Get Data")
+            Debug.log(toString allResponses)
+
+    in
+    allResponses
 
 
 myVis : Spec
 myVis =
     let
+        _=
+            -- Do something here with some data eventually!
+            getData
         des =
             description "SCDS Assessment"
 
         data =
             dataFromColumns []
-                << dataColumn "a" (strs [ "A", "B", "C" ])
-                << dataColumn "b" (nums [ 28, 55, 43 ])
+                << dataColumn "a" (strs [ "Org Values"
+                , "Org Behaves"
+                , "Definition"
+                , "Information"
+                , "Operations"
+                , "Technology"
+                , "People"
+                , "Risk"
+                , "Accountability"
+                , "Performance"
+                ])
+                << dataColumn "b" (nums [ 5, 4, 8 ])
         enc =
             encoding
                 << position X [ pName "b", pMType Quantitative ]
