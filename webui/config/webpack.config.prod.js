207b326b31d61841890791b1887f4cefa1c5b6f2
Elliot Murphy <statik@users.noreply.github.com>
2018-12-25 12:40:41 -0500

    Major upgrade to webpack 4.
    npm run-script build now runs enough to show us the Elm
    compiler errors remaining from the Elm 19 upgrade.
    
    Still need to fix npm run-script start.
    
    Signed-off-by: Elliot Murphy <statik@users.noreply.github.com>

--- webui/config/webpack.config.prod.js
+++ webui/config/webpack.config.prod.js
@@ -1,14 +1,16 @@
 'use strict';
 
 const autoprefixer = require('autoprefixer');
-const DefinePlugin = require('webpack/lib/DefinePlugin');
-const UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
+const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const AssetsPlugin = require('assets-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
 const InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
-const getClientEnvironment = require('./env');
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
 const paths = require('../config/paths');
+const getClientEnvironment = require('./env');
 
 // Webpack uses `publicPath` to determine where the app is being served from.
 // It requires a trailing slash, or the file assets will get an incorrect path.
@@ -16,6 +18,8 @@ const publicPath = paths.servedPath;
 // Some apps do not use client-side routing with pushState.
 // For these, "homepage" can be set to "." to enable relative asset paths.
 const shouldUseRelativeAssetPaths = publicPath === './';
+// Source maps are resource heavy and can cause out of memory issue for large source files.
+const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== 'false';
 // `publicUrl` is just like `publicPath`, but we will provide it to our app
 // as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
 // Omit trailing slash as %PUBLIC_URL%/xyz looks better than %PUBLIC_URL%xyz.
@@ -23,6 +27,8 @@ const publicUrl = publicPath.slice(0, -1);
 
 // Get environment variables to inject into our app.
 const env = getClientEnvironment(publicUrl);
+
+const useDebugger = process.env.ELM_DEBUGGER === 'true';
 
 // Note: defined here because it will be used more than once.
 const cssFilename = 'static/css/[name].[contenthash:8].css';
@@ -33,35 +39,106 @@ const cssFilename = 'static/css/[name].[contenthash:8].css';
 // To have this structure working with relative paths, we have to use custom options.
 const extractTextPluginOptions = shouldUseRelativeAssetPaths
   ? // Making sure that the publicPath goes back to to build folder.
-    { publicPath: Array(cssFilename.split('/').length).join('../') }
+  { publicPath: Array(cssFilename.split('/').length).join('../') }
   : {};
 
 module.exports = {
+  mode: 'production',
   // Don't attempt to continue if there are any errors.
   bail: true,
-
-  entry: [paths.appIndexJs],
-
+  // We generate sourcemaps in production. This is slow but gives good results.
+  // You can exclude the *.map files from the build during deployment.
+  devtool: shouldUseSourceMap ? 'source-map' : false,
+  // In production, we only want to load the polyfills and the app code.
+  entry: [require.resolve('./polyfills'), paths.appIndexJs],
   output: {
     // The build folder.
     path: paths.appBuild,
-
-    // Append leading slash when production assets are referenced in the html.
+    // Generated JS file names (with nested folders).
+    // There will be one main bundle, and one file per asynchronous chunk.
+    // We don't currently advertise code splitting but Webpack supports it.
+    filename: 'static/js/[name].[chunkhash:8].js',
+    chunkFilename: 'static/js/[name].[chunkhash:8].chunk.js',
+    // We inferred the "public path" (such as / or /my-project) from homepage.
     publicPath: publicPath,
-
-    // Add /* filename */ comments to generated require()s in the output.
-    pathinfo: true,
-
-    // Generated JS files.
-    filename: 'static/js/[name].[chunkhash:8].js'
+    // Point sourcemap entries to original disk location (format as URL on Windows)
+    devtoolModuleFilenameTemplate: info =>
+      path.relative(paths.appSrc, info.absoluteResourcePath).replace(/\\/g, '/')
   },
-
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          // ES5 is required in the minified code if you want compatibility with IE11,
+          // otherwise you can bump it up to ES8
+          ecma: 5,
+          // Compression settings mostly based on <https://guide.elm-lang.org/optimization/asset_size.html>
+          compress: {
+            passes: 2,
+            warnings: false,
+            // Disabled because of an issue with Uglify breaking seemingly valid code:
+            // https://github.com/facebook/create-react-app/issues/2376
+            // Pending further investigation:
+            // https://github.com/mishoo/UglifyJS2/issues/2011
+            comparisons: false,
+            pure_getters: true,
+            keep_fargs: false,
+            unsafe_comps: true,
+            unsafe: true,
+            pure_funcs: [
+              'A2',
+              'A3',
+              'A4',
+              'A5',
+              'A6',
+              'A7',
+              'A8',
+              'A9',
+              'F2',
+              'F3',
+              'F4',
+              'F5',
+              'F6',
+              'F7',
+              'F8',
+              'F9'
+            ]
+          },
+          mangle: {
+            safari10: true
+          },
+          output: {
+            comments: false,
+            // Turned on because emoji and regex is not minified properly using default
+            // https://github.com/facebook/create-react-app/issues/2488
+            ascii_only: true
+          }
+        },
+        // Use multi-process parallel running to improve the build speed
+        // Default number of concurrent runs: os.cpus().length - 1
+        parallel: true,
+        // Enable file caching
+        cache: true,
+        sourceMap: shouldUseSourceMap
+      })
+    ],
+    // Automatically split vendor and commons
+    // https://twitter.com/wSokra/status/969633336732905474
+    splitChunks: {
+      chunks: 'all'
+    },
+    // Keep the runtime chunk seperated to enable long term caching
+    // https://twitter.com/wSokra/status/969679223278505985
+    runtimeChunk: true
+  },
   resolve: {
     modules: ['node_modules'],
     extensions: ['.js', '.elm']
   },
 
   module: {
+    strictExportPresence: true,
+
     noParse: /\.elm$/,
 
     rules: [
@@ -71,13 +148,58 @@ module.exports = {
         loader: require.resolve('babel-loader'),
         query: {
           presets: [
-            require.resolve('babel-preset-es2015'),
-            require.resolve('babel-preset-es2016'),
-            require.resolve('babel-preset-es2017')
+            [
+              require.resolve('@babel/preset-env'),
+              {
+                // `entry` transforms `@babel/polyfill` into individual requires for
+                // the targeted browsers. This is safer than `usage` which performs
+                // static code analysis to determine what's required.
+                // This is probably a fine default to help trim down bundles when
+                // end-users inevitably import '@babel/polyfill'.
+                useBuiltIns: 'entry',
+                // Do not transform modules to CJS
+                modules: false
+              }
+            ]
+          ],
+          plugins: [
+            // Polyfills the runtime needed for async/await and generators
+            [
+              require('@babel/plugin-transform-runtime').default,
+              {
+                helpers: false,
+                regenerator: true
+              }
+            ]
           ]
         }
       },
-
+      // Process any JS outside of the app with Babel.
+      // Unlike the application JS, we only compile the standard ES features.
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: require.resolve('babel-loader'),
+            options: {
+              babelrc: false,
+              compact: false,
+              presets: [
+                [
+                  // Latest stable ECMAScript features
+                  require('@babel/preset-env').default,
+                  {
+                    // Do not transform modules to CJS
+                    modules: false
+                  }
+                ]
+              ],
+              cacheDirectory: true,
+              highlightCode: true
+            }
+          }
+        ]
+      },
       {
         test: /\.elm$/,
         exclude: [/elm-stuff/, /node_modules/],
@@ -89,64 +211,69 @@ module.exports = {
           {
             loader: require.resolve('string-replace-loader'),
             query: {
-                  search: '%PUBLIC_URL%',
-                  replace: publicUrl
+              search: '%PUBLIC_URL%',
+              replace: publicUrl
             }
           },
           {
-            // Use the local installation of elm-make
+            // Use the local installation of elm make
             loader: require.resolve('elm-webpack-loader'),
             options: {
-              pathToMake: paths.elmMake
+              // If ELM_DEBUGGER was set to "true", enable it. Otherwise
+              // for invalid values, "false" and as a default, disable it
+              debug: useDebugger,
+              optimize: !useDebugger,
+              pathToElm: paths.elm
             }
           }
         ]
       },
+
+      // "postcss" loader applies autoprefixer to our CSS.
+      // "css" loader resolves paths in CSS and adds assets as dependencies.
+      // `MiniCSSExtractPlugin` extracts styles into CSS
+      // files. If you use code splitting, async bundles will have their own separate CSS chunk file.
+      // By default we support CSS Modules with the extension .module.css
       {
         test: /\.scss$/,
-        use: ExtractTextPlugin.extract(
-          Object.assign(
-            {
-              fallback: require.resolve('style-loader'),
-              use: [
-                {
-                  loader: require.resolve('css-loader'),
-                  options: {
-                    minimize: true
-                  }
-                },
-                {
-                  loader: require.resolve('postcss-loader'),
-                  options: {
-                    ident: 'postcss', // https://webpack.js.org/guides/migrating/#complex-options
-                    plugins: () => [
-                      autoprefixer({
-                        browsers: [
-                          '>1%',
-                          'last 4 versions',
-                          'Firefox ESR',
-                          'not ie < 9'
-                        ]
-                      })
-                    ]
-                  }
-                },
-                {
-                  loader: require.resolve('sass-loader')
-                }
+        exclude: /\.module\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: require.resolve('css-loader'),
+            options: {
+              importLoaders: 1,
+              minimize: true,
+              sourceMap: shouldUseSourceMap
+            }
+          },
+          {
+            loader: require.resolve('postcss-loader'),
+            options: {
+              // Necessary for external CSS imports to work
+              // https://github.com/facebook/create-react-app/issues/2677
+              ident: 'postcss',
+              plugins: () => [
+                require('postcss-flexbugs-fixes'),
+                autoprefixer({
+                  flexbox: 'no-2009'
+                })
               ]
-            },
-            extractTextPluginOptions
-          )
-        )
+            }
+          },
+          {
+            loader: require.resolve('sass-loader')
+          }
+        ]
       },
+
       {
         exclude: [/\.html$/, /\.js$/, /\.elm$/, /\.scss$/, /\.json$/, /\.svg$/],
         loader: require.resolve('url-loader'),
         options: {
           limit: 10000,
           name: 'static/media/[name].[hash:8].[ext]'
-      }
+        }
       },
       // "file" loader for svg
       {
@@ -160,27 +287,7 @@ module.exports = {
   },
 
   plugins: [
-    new AssetsPlugin({ path: paths.appBuild }),
-
-    new DefinePlugin(env.stringified),
-
-    new InterpolateHtmlPlugin(env.raw),
-
-    // Minify the compiled JavaScript.
-    new UglifyJsPlugin({
-      compress: {
-        warnings: false
-      },
-      output: {
-        comments: false
-      }
-    }),
-
-    // TODO: with the html-webpack-plugin present, we are getting a crash
-    // during build. commenting out this plugin avoids the crash, but then
-    // we don't get a generated index.html with the appropriate links
-    // injected to load the compiled css and js bundles.
-
+    // Generates an `index.html` file with the <script> injected.
     new HtmlWebpackPlugin({
       inject: true,
       template: paths.appHtml,
@@ -197,10 +304,72 @@ module.exports = {
         minifyURLs: true
       }
     }),
+    // Makes some environment variables available in index.html.
+    // The public URL is available as %PUBLIC_URL% in index.html, e.g.:
+    // <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
+    // In production, it will be an empty string unless you specify "homepage"
+    // in `package.json`, in which case it will be the pathname of that URL.
+    new InterpolateHtmlPlugin(HtmlWebpackPlugin, env.raw),
+    // Makes some environment variables available to the JS code, for example:
+    // if (process.env.NODE_ENV === 'production') { ... }. See `./env.js`.
+    // It is absolutely essential that NODE_ENV was set to production here.
+    // Otherwise React will be compiled in the very slow development mode.
+    new webpack.DefinePlugin(env.stringified),
 
-    // Note: this won't work without ExtractTextPlugin.extract(..) in `loaders`.
-    new ExtractTextPlugin({
-      filename: cssFilename
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: 'static/css/[name].[contenthash:8].css',
+      chunkFilename: 'static/css/[name].[contenthash:8].chunk.css'
+    }),
+    // Generate a manifest file which contains a mapping of all asset filenames
+    // to their corresponding output file so that tools can pick it up without
+    // having to parse `index.html`.
+    new ManifestPlugin({
+      fileName: 'asset-manifest.json',
+      publicPath: publicPath
+    }),
+    // Generate a service worker script that will precache, and keep up to date,
+    // the HTML & assets that are part of the Webpack build.
+    new SWPrecacheWebpackPlugin({
+      // By default, a cache-busting query parameter is appended to requests
+      // used to populate the caches, to ensure the responses are fresh.
+      // If a URL is already hashed by Webpack, then there is no concern
+      // about it being stale, and the cache-busting can be skipped.
+      dontCacheBustUrlsMatching: /\.\w{8}\./,
+      filename: 'service-worker.js',
+      logger(message) {
+        if (message.indexOf('Total precache size is') === 0) {
+          // This message occurs for every build and is a bit too noisy.
+          return;
+        }
+        if (message.indexOf('Skipping static resource') === 0) {
+          // This message obscures real errors so we ignore it.
+          // https://github.com/facebook/create-react-app/issues/2612
+          return;
+        }
+        console.log(message);
+      },
+      minify: true,
+      // Don't precache sourcemaps (they're large) and build asset manifest:
+      staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/]
+      // `navigateFallback` and `navigateFallbackWhitelist` are disabled by default; see
+      // https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/template/README.md#service-worker-considerations
+      // navigateFallback: publicUrl + '/index.html',
+      // navigateFallbackWhitelist: [/^(?!\/__).*/],
     })
-  ]
+  ],
+  // Some libraries import Node modules but don't use them in the browser.
+  // Tell Webpack to provide empty mocks for them so importing them works.
+  node: {
+    dgram: 'empty',
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    child_process: 'empty'
+  },
+
+  // Turn off performance processing because we utilize
+  // our own hints via the FileSizeReporter
+  performance: false
 };
