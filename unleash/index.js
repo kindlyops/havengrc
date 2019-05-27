305342eccd695ae69e125679ad5177a0a32baccf
Elliot Murphy <statik@users.noreply.github.com>
2019-05-27 18:18:11 -0400

    Prep work for keycloak-gatekeeper
    Signed-off-by: Elliot Murphy <statik@users.noreply.github.com>

--- unleash/index.js
+++ unleash/index.js
@@ -5,10 +5,34 @@ const express = require("express");
 const unleash = require('unleash-server');
 
 let options = {};
-//options.adminAuthentication = 'unsecure';
-options.adminAuthentication = 'none';
-//options.preRouterHook = basicAuth;
-//options.preRouterHook = enableKeycloakOauth
+
+options.adminAuthentication = 'custom';
+function gatekeeperAuthentication(app) {
+    app.use('/api/admin/', (req, res, next) => {
+        const email = req.get('X-Auth-Email');
+
+        if (email) {
+            // TODO: need to do some verification of credentials here, probably
+            // validate X-Auth-Token signature
+
+            const user = new unleash.User({ email: `${email}` });
+            req.user = user;
+            next();
+        } else {
+            return res
+                .status('401')
+                .end('access denied');
+        }
+    });
+
+    app.use((req, res, next) => {
+        // Updates active sessions every hour
+        req.session.nowInHours = Math.floor(Date.now() / 3600e3);
+        next();
+    });
+};
+options.preRouterHook = gatekeeperAuthentication;
+
 
 function serveFrontend(app) {
     app.use('/', express.static('/frontend'));
