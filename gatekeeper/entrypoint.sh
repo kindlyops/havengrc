5f492618186ef1e9f25a7add05b1780b6652c282
Elliot Murphy <statik@users.noreply.github.com>
2019-06-03 11:26:33 -0400

    Newer shell syntax for codacy
    Signed-off-by: Elliot Murphy <statik@users.noreply.github.com>

--- gatekeeper/entrypoint.sh
+++ gatekeeper/entrypoint.sh
@@ -3,7 +3,7 @@
 set -x
 
 # get hostname of keycloak auth server
-KEYCLOAK=`echo "$DISCOVERY_URL" | awk -F/ '{print $3}'`
+KEYCLOAK=$(echo "$DISCOVERY_URL" | awk -F/ '{print $3}')
 
 # wait for keycloak to be available and then template the config file
 exec /usr/local/bin/dockerize -wait http://$KEYCLOAK -timeout 60s -template /opt/config/gatekeeper.tmpl:/opt/gatekeeper.yaml /opt/keycloak-gatekeeper --config /opt/gatekeeper.yaml --verbose
