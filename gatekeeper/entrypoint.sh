f1e796368251d8b6107b3186f88ee7f91b00b316
Elliot Murphy <statik@users.noreply.github.com>
2019-06-04 00:59:26 -0400

    trying https in local proxy
    Signed-off-by: Elliot Murphy <statik@users.noreply.github.com>

--- gatekeeper/entrypoint.sh
+++ gatekeeper/entrypoint.sh
@@ -9,7 +9,7 @@ set -x
 /usr/local/bin/dockerize -wait $KEYCLOAK_INTERNAL -timeout 60s
 /usr/local/bin/dockerize -wait $DISCOVERY_URL
 
-curl -i $DISCOVERY_URL
+curl -k -i $DISCOVERY_URL
 
 # wait for keycloak to be available and then template the config file
 exec /usr/local/bin/dockerize -template /opt/config/gatekeeper.tmpl:/opt/gatekeeper.yaml /opt/keycloak-gatekeeper --config /opt/gatekeeper.yaml
